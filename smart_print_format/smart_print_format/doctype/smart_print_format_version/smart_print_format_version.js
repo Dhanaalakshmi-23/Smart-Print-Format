a3af78e91b0c1c856d9b8553609161dcb99ce4fa
Dhanaalakshmi-23 <dhanaalakshminarayanan@gmail.com>
2026-09-21 12:58:15 +0530

    feat: add smart print format doctypes

---
+++ smart_print_format/smart_print_format/doctype/smart_print_format_version/smart_print_format_version.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Dhanaa Lakshmi and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Smart Print Format Version", {
+// 	refresh(frm) {
+
+// 	},
+// });
